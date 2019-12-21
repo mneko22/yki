15712952cba63327c5ae9787c998c2e3eb4b212f
NaokiKuramochi <kuramochi@cloud-ace.jp>
2019-12-22 01:17:38 +0900

    Mod. Pub/Subとの連携見直し

--- applications/update/web/api/motions.go
+++ applications/update/web/api/motions.go
@@ -6,8 +6,12 @@ import "net/http"
 import "yki/src/gateways/controllers"
 import "yki/src/gateways/repositories"
 
-type Request struct {
-	Motion bool `json:"motion"`
+type PubSubMessage struct {
+	Message struct {
+		Data []byte `json:"data,omitempty"`
+		ID   string `json:"id"`
+	} `json:"message"`
+	Subscription string `json:"subscription"`
 }
 
 func Liten() {
@@ -29,13 +33,13 @@ func postMotions(c *gin.Context) {
 	mrepo := repositories.NewFirestoreMotionRepository("ca-camp-rabbit-team-2019-12")
 	mc := controllers.NewMotionController(mrepo)
 	req := load(c)
-	m := mc.ConverToMotion(req.Motion)
+	m := mc.ConverToMotion(req.Message.Data.motion.(bool))
 	mc.Update(m)
 	c.JSON(http.StatusOK, mc.MotionToHTTPResponse(mc.Get()))
 }
 
-func load(c *gin.Context) Request{
-	var req Request
+func load(c *gin.Context) PubSubMessage{
+	var req PubSubMessage
 	c.BindJSON(&req)
 	return req
 }
